646555282c69331fb387cd165ee4218914d258df
Alberto Cavalin <alberto.cavalin@unipd.it>
2024-10-15 11:56:54 +0200

    easystaff.user.js - EasyRoom - prenotazioni in sospeso: incrocia dati della mappa

--- easystaff.user.js
+++ easystaff.user.js
@@ -7,10 +7,15 @@
 // @include     https://gestionedidattica.unipd.it/PresenzeStudenti/Corsi.php*
 // @icon        data:image/x-icon;base64,R0lGODlhEAAQAMQAAP/////39/f39/fv7+/v7/fe3t7e3ua9vb29va2trdaclJycnISEhHt7e71aSmNjY7VCKVJSUkJCQjo6OjExMQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAAHAP8ALAAAAAAQABAAAAVkICCOZCkeSqoEJulAMFS041vcY0PtUSK+pZ1w94tBAIydD7CgFB1QQIQ4IhRJj90kYa0dR4ghZdJ9QR0DpJgiALxiM4AgkVW2DKQwZdGKNEYJOwh9PBI7EjQJE0MRNCMCBngjIQA7
 // @downloadURL https://github.com/acavalin/tp_unipd/raw/main/easystaff.user.js
-// @version     1.3.0
-// @grant       none
+// @version     1.4.0
+// @grant       GM_xmlhttpRequest
+// @connect     didattica.unipd.it
+// @connect     localhost
 // @license     GPLv3
 // ==/UserScript==
+
+// https://www.tampermonkey.net/documentation.php?locale=en#api:GM_xmlhttpRequest
+// https://www.tampermonkey.net/documentation.php?locale=en#meta:connect
 
 // NB: queste due direttive non servono perche' la pagina ha gia' jQuery
 // require     https://code.jquery.com/jquery-3.7.1.min.js
@@ -26,105 +31,182 @@
           'border-radius: 0.5rem; border: 1px outset red;';
     $('<div id="acavalin" style="'+links_css+'">'+links_script+' by '+links_hp+'</div>').appendTo('body');
   }//if
-  
-  $.er_enh = {
+
+  $.easy_enh = {
     serialize_inputs: function (inputs) {
       var params = {};
       $.map(inputs.serializeArray(), function(p,i) { params[p.name] = p.value; });
       return params;
-    }//serialize_inputs
-  };//$.er_enh
-  
-  // MODA
+    },//serialize_inputs
+
+    add_error: function (msg, target, options) {
+      options ||= {}
+      var div = $('<div style="font-weight: bold; background-color: '+(options.color || 'yellow')+'; padding: 0.2rem 0.4rem; margin-bottom: 0.2rem; border: 2px solid red; text-align: left; white-space: pre-wrap;"></div>');
+      options.html ? div.html(msg) : div.text(msg);
+      div.insertBefore(target);
+    },//add_error
+    
+    er: {
+      cerca_assegnazioni_su_ufficio: function (mappa) {
+        $('table#confirmReservationsListCheck tr:gt(0)').each(function (i, el) {
+          var tr = $(el);
+          var giorno       = tr.find('td:eq(2)').text().trim().substr(0,2);
+          var [da, a]      = tr.find('td:eq(3)').text().trim().split("\n")[0].substr(12).split(' - ');
+          var [aula, sede] = tr.find('td:eq(6)').text().replace(/(.+) \[(.+)\]/, "$1\t$2").split("\t");
+          var fasce        = ((mappa.ass[sede] || {})[aula] || {})[giorno] || [];
+          
+          // inserisci mailto link
+          var td_mail = tr.find('td:eq(7)');
+          var [user, mail] = tr.find('td:eq(7)').text().trim().replace(/(.+) \((.+)\)/, "$1\t$2").split("\t");
+          if (mail)
+            $(`<a href="mailto:${mail}" target="_blank" title="invia email a\n${mail}">@${user}</a>`).appendTo( td_mail.empty() );
+          
+          var strutt_utente = mappa.utenti[mail] || [];
+          td_mail.find('a').after( $('<i/>').text(strutt_utente.join(', ')) ).after('<br>');
+          
+          // prenotazione.inizio < fascia.fine && prenotazione.fine > fascia.inizio
+          var num_strutt_non_proprie = 0;
+          var strutture = fasce.
+            filter(function (f, i) { return f.da < a && f.a > da; }).
+            map(function(el,i){
+              if (!strutt_utente.includes(el.s))
+                num_strutt_non_proprie += 1;
+              return `${el.s} (${el.da}~${el.a})`;
+            }).join("\n")
+          
+          var td_dr = tr.find('td:eq(9)').css('white-space', 'nowrap').find('br').replaceWith(' ').end();
+          var msg = $('<div></div>').text( td_dr.text() ).appendTo( td_dr.empty() );
+          if (strutture != '')
+            $.easy_enh.add_error(strutture, msg, {color: num_strutt_non_proprie == 0 ? 'lightgreen' : 'orange'});
+          
+          //console.log([strutture, fasce, giorno, da, a, aula, sede]);
+        });
+      }//cerca_assegnazioni_su_ufficio
+    }//$.easy_enh.er
+  };//$.easy_enh
+
+
+  // --- EasyRoom --------------------------------------------------------------
+  if (location.pathname == '/Aule/index.php') {
+    // riabilita F5 per ricaricare la pagina
+    if (typeof disable_f5 == 'function')
+      $(document).off("keydown", disable_f5);
+
+    if (location.search.includes('content=gestore_conferma_prenotazioni')) {
+      var msg_app_uff = 'Errore lettura assegnazioni dall\'app <a href="https://ufficio.didattica.unipd.it" target="_blank">ufficio</a>! (Hai fatto il login?)';
+      
+      // allinea celle a sx
+      $('table#confirmReservationsListCheck tr:gt(0) td').
+        css('text-align', 'left').css('border-left', '1px dotted silver');
+      
+      // carica assegnazioni dall'app ufficio.didattica.unipd.it
+      GM_xmlhttpRequest({
+        method: 'GET',
+        url: 'https://ufficio.didattica.unipd.it/er/bacheca/mappa_assegnazioni.json',
+        nocache: true,
+        responseType: 'json',
+        onload:  function (resp) {
+          if (resp.status == 200) {
+            $.easy_enh.mappa = resp.response;
+            $.easy_enh.er.cerca_assegnazioni_su_ufficio(resp.response);
+            console.log("app ufficio: assegnazioni salvate in $.easy_enh.mappa");
+          } else
+            $.easy_enh.add_error(msg_app_uff, 'table#confirmReservationsListCheck', {html: true});
+        },//onload
+        onerror: function () { $.easy_enh.add_error(msg_app_uff, 'table#confirmReservationsListCheck', {html: true}); },
+        onabort: function () { $.easy_enh.add_error(msg_app_uff, 'table#confirmReservationsListCheck', {html: true}); },
+      });
+    }// EasyRoom -- location includes "content=gestore_scuole"
+
+    if (location.search.includes('content=gestore_scuole')) {
+      function get_csv_by_row(image_id) {
+        var lbl_scuola = $('img#'+image_id).parent().prev().text().trim(),
+            box_id = image_id.replace('parent_', ''),
+            csv_text = '';
+
+        $(".infoBox[id='"+box_id+"']").find('p + ul').each(function (ul_idx) {
+           var lbl_strutt = $(this).prev().text().replace('Struttura didattica: ', '').trim();
+           csv_text += "\n" + $(this).children('li').map(function () {
+             return lbl_scuola + "\t" + lbl_strutt + "\t" + $(this).text() + "\t" + $(this).text().substr(-7,6);
+           }).get().join("\n");
+        });
+
+        return csv_text.trim();
+      }//get_csv_by_row
+
+      $.easy_enh.copy_csv = function (image_id) {
+        navigator.clipboard.writeText( get_csv_by_row(image_id) );
+      }//$.easy_enh.copy_csv
+
+      $.easy_enh.copy_all_csv = function () {
+        var csv_text = ''
+        $('td img[id^="parent_details_EC_"]').each(function (img_idx) { csv_text += "\n" + get_csv_by_row( $(this).attr('id') ); });
+        navigator.clipboard.writeText(csv_text.trim());
+      }//$.easy_enh.copy_all_csv
+
+      // inserisci pulsante per copiare un CSV del popup con l'elenco dei corsi per ogni struttura
+      $('td img[id^="parent_details_EC_"]').each(function (img_idx) {
+        $(this).after('<a href="#" onclick="$.easy_enh.copy_csv(\''+$(this).attr('id')+'\'); return false" title="copia negli appunti un CSV dei dati mostrati nel popup">CSV</a>');
+      });
+
+      // inserisci pulsante per copiare un CSV coi dati da tutti i popup di ogni scuola
+      $('table#ScuoleListCheck tr:first th:nth-child(2)').
+        after('<a href="#" onclick="$.easy_enh.copy_all_csv(); return false" title="copia negli appunti un CSV con tutti i dati nei popup delle scuole">CSV</a>');
+    }// EasyRoom -- location includes "content=gestore_scuole"
+  }// EasyRoom
+
+
+  // --- MODA ------------------------------------------------------------------
   if (location.pathname.includes('/MODA/') && location.search.includes('content=gestore_ugov_import_launch')) {
     // nascondi pulsanti distruttivi mai usati, e aggiungi link per il loro toggle
     $(':submit').filter('[name="_qf_importUGOVForm_import_singolo"], [name="_qf_importUGOVForm_import_new_singolo"]').addClass('bt-import').hide();
     $('table#structuresTable tr:first th:nth-child(6)').
       append('<a href="#" onclick="jQuery(\':submit.bt-import\').toggle(); return false">👀<br>toggle</a>');
-    
+
     //// sincronizza tutto in sequenza
-    //// @grant       GM_xmlhttpRequest
-    //$.er_enh.sync_next = function () {
-    //  var data = $.er_enh.serialize_inputs($('#importUGOVForm input:not(:submit)'));
+    //$.easy_enh.sync_next = function () {
+    //  var data = $.easy_enh.serialize_inputs($('#importUGOVForm input:not(:submit)'));
     //  var bt = $(':input[name="_qf_importUGOVForm_update_singolo"]:not(.sync-done):first');
     //
     //  var cur_data = $.extend({}, data); // clona dati di partenza
     //  cur_data.objectID = bt.attr('onclick').replace(/.+, *([0-9]+)\).*/, '$1');
     //  cur_data._qf_importUGOVForm_update_singolo = bt.val();
-    //  
+    //
     //  var tot  = $(':input[name="_qf_importUGOVForm_update_singolo"]').length;
     //  var done = $(':input[name="_qf_importUGOVForm_update_singolo"].sync-done').length + 1;
     //  $('#syncall-counter').text(' '+done+'/'+tot);
-    //  
+    //
     //  console.log(bt.parents('tr:first').find('td:first').text());
     //  console.log(cur_data);
-    //  
+    //
     //  GM_xmlhttpRequest({
     //    method: 'POST',
     //    data: cur_data,
     //    url: $('#importUGOVForm').attr('action'),
-    //    onload:  $.er_enh.sync_next,
+    //    onload:  $.easy_enh.sync_next,
     //    onerror: function () { alert('ERRORE: alla riga '+bt.parents('tr:first').find('td:first').text()); },
     //    onabort: function () { alert('ERRORE: alla riga '+bt.parents('tr:first').find('td:first').text()); }
     //  });
-    //}//$.er_enh.sync_all
-    //// aggiungi pulsante per chiamare $.er_enh.sync_all()
-    //$('table#structuresTable tr:first th:nth-child(5)').
-    //  append('<a href="#" onclick="$.er_enh.sync_next(); return false">♻️<br>sync-all</a><span id="syncall-counter"></span>');
+    //}//$.easy_enh.sync_all
+    //
+    //if (location.search.includes('sync_all=1')) $.easy_enh.sync_next();
   }// MODA -- content=gestore_ugov_import_launch
 
-  // EasyRoom
-  if (location.search.includes('content=gestore_scuole')) {
-    function get_csv_by_row(image_id) {
-      var lbl_scuola = $('img#'+image_id).parent().prev().text().trim(),
-          box_id = image_id.replace('parent_', ''),
-          csv_text = '';
 
-      $(".infoBox[id='"+box_id+"']").find('p + ul').each(function (ul_idx) {
-         var lbl_strutt = $(this).prev().text().replace('Struttura didattica: ', '').trim();
-         csv_text += "\n" + $(this).children('li').map(function () {
-           return lbl_scuola + "\t" + lbl_strutt + "\t" + $(this).text() + "\t" + $(this).text().substr(-7,6);
-         }).get().join("\n");
-      });
-
-      return csv_text.trim();
-    }//get_csv_by_row
-
-    $.er_enh.copy_csv = function (image_id) {
-      navigator.clipboard.writeText( get_csv_by_row(image_id) );
-    }//$.er_enh.copy_csv
-
-    $.er_enh.copy_all_csv = function () {
-      var csv_text = ''
-      $('td img[id^="parent_details_EC_"]').each(function (img_idx) { csv_text += "\n" + get_csv_by_row( $(this).attr('id') ); });
-      navigator.clipboard.writeText(csv_text.trim());
-    }//$.er_enh.copy_all_csv
-
-    // inserisci pulsante per copiare un CSV del popup con l'elenco dei corsi per ogni struttura
-    $('td img[id^="parent_details_EC_"]').each(function (img_idx) {
-      $(this).after('<a href="#" onclick="$.er_enh.copy_csv(\''+$(this).attr('id')+'\'); return false" title="copia negli appunti un CSV dei dati mostrati nel popup">CSV</a>');
-    });
-
-    // inserisci pulsante per copiare un CSV coi dati da tutti i popup di ogni scuola
-    $('table#ScuoleListCheck tr:first th:nth-child(2)').
-      after('<a href="#" onclick="$.er_enh.copy_all_csv(); return false" title="copia negli appunti un CSV con tutti i dati nei popup delle scuole">CSV</a>');
-  }// EasyRoom -- location includes "content=gestore_scuole"
-  
-  
-  // EasyBadge
+  // --- EasyBadge -------------------------------------------------------------
   if (location.search.includes('action=FrequenzeLezione') && location.search.includes('id_lezione=')) {
     var posti = $('input.form-control[name="posto"]'),
         tbody = posti.eq(0).parents('tbody:first');
-    
+
     // evidenzia righe con lo stesso codice posto
     for (var i = 0; i < posti.length; i++) {
       if (posti.eq(i).data('doppione') || posti.eq(i).val().toLowerCase() == 'xxx')
         continue;
-      
+
       for (var j = i+1; j < posti.length; j++) {
         if (posti.eq(j).data('doppione') || posti.eq(j).val().toLowerCase() == 'xxx')
           continue;
-        
+
         if (posti.eq(i).val() == posti.eq(j).val())
           posti.eq(i).add( posti.eq(j) ).
             data('doppione', true).
